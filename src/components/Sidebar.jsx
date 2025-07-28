ef02961894f770858e03b33eb6c858ff2efe21b2
ErikaGeraldine <geraldinevaldez120@gmail.com>
2025-07-28 00:11:45 -0300

    feat: se modifica el  diseño del Dashboard y se agrega el componente menu lateral  Sidebar.

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Sidebar.css';
+
+const Sidebar = () => {
+  return (
+    <aside className="sidebar">
+      <nav className="sidebar-nav">
+        <ul>
+          <li><a href="/dashboard">Inicio</a></li>
+          <li><a href="/carga-propuesta">Cargar Propuesta</a></li>
+          <li><a href="/carga-trabajo">Cargar Trabajo</a></li>
+          <li><a href="#">Consultar Estado</a></li>
+          <li><a href="#">Subir Entregas</a></li>
+          <li><a href="#">Observaciones</a></li>
+          <li><a href="#">Resoluciones</a></li>
+        </ul>
+      </nav>
+    </aside>
+  );
+};
+
+export default Sidebar;
