c71c957b4b854470ea151663123304e49c495a4d
ErikaValdez <geraldinevaldez120@gmail.com>
2026-07-20 21:41:06 -0300

    feat: conexion register student

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -25,3 +25,12 @@ declare module '*.svg' {
   const src: string;
   export default src;
 }
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
